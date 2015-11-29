517aed3a2157bc91adaeaec91b6395750fc6c4aa
tohrinagi <tohrinagi@gmail.com>
2015-11-29 18:10:52 +0900

    WIP issue #5

--- js/options.js
+++ js/options.js
@@ -1,12 +1,33 @@
+/*exported storage*/
 var url = document.getElementById('url');
 var token = document.getElementById('token');
 
 document.getElementById('save').onclick = function() {
-  localStorage['url'] = url.value;
-  localStorage['token'] = token.value;
+  url = url.value;
+  token = token.value;
+  if( url.slice(-1) != '/' )
+  {
+    url += '/';
+  }
+
+  $.ajax({
+    url: url + "account",
+    cache: false,
+    type: 'GET',
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", "Bearer " + token);
+    },
+    success: function(json) {
+        storage.register( url, token, json.username, json.id, json.avatar );
+        alert(json);
+      },
+    error: function() {
+        alert("error occred!");
+      }
+  });
 };
 
 document.body.onload = function() {
-  url.value = localStorage['url'] ? localStorage['url'] : "";
-  token.value = localStorage['token'] ? localStorage['token']: "";
+  url.value = storage.url() ? storage.url() : "";
+  token.value = storage.token() ? storage.token() : "";
 };
